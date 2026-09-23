a59f832df1aa01ea5280bcd2023079c18e3716c2
Lucas <luema94@gmail.com>
2026-09-22 22:41:06 -0300

    fix: se hicieron modificaciones en animaciones

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,53 +1,61 @@
-import cloudy from "../assets/cloudy.png"
-import rain from "../assets/rain.png"
-import sun from "../assets/sun.png"
-import thunderstorm from "../assets/thunderstorm.png"
-import snow from "../assets/snow.png"
-import mist from "../assets/mist.png"
+import cloudy from "../animtations/clouds.json"
+import rain from "../animtations/rain.json"
+import sun from "../animtations/clear.json"
+import thunderstorm from "../animtations/storm.json"
+import snow from "../animtations/snow.json"
+import mist from "../animtations/mist.json"
 import { CloudHail } from 'lucide-react';
+import Lottie from "lottie-react"
 
-export const Forecast = ({forecast, dailyForecast})=>{
+export const Forecast = ({ forecast, dailyForecast }) => {
 
-const weatherIconMap = {
-  Clear: sun,
-  Clouds: cloudy,
-  Rain: rain,
-  Thunderstorm: thunderstorm,
-  Snow: snow,
-  Mist: mist
-};
+    const weatherIconMap = {
+        Clear: sun,
+        Clouds: cloudy,
+        Rain: rain,
+        Thunderstorm: thunderstorm,
+        Snow: snow,
+        Mist: mist
+    };
 
-const data = dailyForecast(forecast)
-console.log(data)
+    const data = dailyForecast(forecast)
+    console.log(data)
 
-    if(!forecast){
+    if (!forecast) {
         return null
     }
-    
+
 
     return (
         <div className="w-full xl:w-2xl max-w-md p-4">
             <h3 className="text-white text-center font-semibold">Pronostico Extendido</h3>
             <div className="flex flex-col gap-3 mt-4">
-            {
-                data.slice(1,5).map((day, index)=>
+                {
+                    data.slice(1, 5).map((day, index) =>
                     (
                         <div key={index} className="rounded-lg flex items-center justify-between p-6 bg-gray-800/70 ">
-                           <img src={weatherIconMap[day.img]} width={28} inline={true} />
-                           <div className="relative flex flex-col justify-center  w-24">
-                           <p className="text-white ">{new Date(day.date).toLocaleDateString("es-ES", { weekday: "long" })}</p>
-                           <div className="absolute top-full flex items-center gap-1 text-blue-400">
-                           <CloudHail className="mt-1" size={16} />
-                           <span className="text-sm">{Math.round(day.pop * 100)}%</span>
-                           </div>
-                           </div>
-                           <p className="text-white ml-1 text-center text-sm">{day.temp_min.toFixed(1)}°</p>
-                           <div className="text-white text-sm">/</div>
-                           <p className="text-red-500 ml-1 text-center text-sm">{day.temp_max.toFixed(1)}°</p>
+                            <div className="w-14 h-14 shrink-0 flex items-center justify-center">
+                                <Lottie
+                                    animationData={weatherIconMap[day.img]}
+                                    loop
+                                    autoplay
+                                    style={{ width: "100%", height: "100%" }}
+                                />
+                            </div>
+                            <div className="relative flex flex-col justify-center  w-24">
+                                <p className="text-white ">{new Date(day.date).toLocaleDateString("es-ES", { weekday: "long" })}</p>
+                                <div className="absolute top-full flex items-center gap-1 text-blue-400">
+                                    <CloudHail className="mt-1" size={16} />
+                                    <span className="text-sm">{Math.round(day.pop * 100)}%</span>
+                                </div>
+                            </div>
+                            <p className="text-white ml-1 text-center text-sm">{day.temp_min.toFixed(1)}°</p>
+                            <div className="text-white text-sm">/</div>
+                            <p className="text-red-500 ml-1 text-center text-sm">{day.temp_max.toFixed(1)}°</p>
                         </div>
                     )
-                )
-            }
+                    )
+                }
             </div>
         </div>
     )
